c143a9c51ae1db822b7740106803c5ce9b699140
João Carlos Belmiro Gonçalves <joao.goncalves@compasso.com.br>
2022-10-05 21:14:13 -0300

    criação de header

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <img src="@/assets/edit_calendar.svg" alt="">
+    <h1>
+        My <span>Notes</span>
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss">
+    header {
+        height: 100px;
+        width: 100%;
+        background-color: $dark;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 2rem;
+
+        h1 {
+            color: $background;
+            letter-spacing: 5px;
+            span {
+              background: linear-gradient(to right,$text-dark , $primary);
+              -webkit-text-fill-color: transparent;
+              -webkit-background-clip: text;
+            }
+        }
+
+        img {
+          padding-left:32px;
+        }
+    }
+</style>
